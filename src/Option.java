e22d95c1108fd07a38d1ca69741a2cb20b060c0b
tektonia <vsilva@deetc.isel.ipl.pt>
2017-06-10 14:45:04 +0100

    Create Option.java

--- src/Option.java
+++ src/Option.java
@@ -20,6 +20,16 @@ import java.io.Serializable;
 
 import ij.IJ;
 
+/**
+ * 
+ * Option
+ * 
+ *   Definition of the options available to the plugin
+ *   The options are stored and retrieved from a file as a serialized object
+ * 
+ * @author Vitor Vaz da Silva
+ *
+ */
 public class Option implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
@@ -52,6 +62,12 @@ public class Option implements Serializable {
 	public String nomeFich="otoring_param.ser";  // Filename for writing the parameters
 	Dados dados=new Dados();
 
+/**
+ * saveParameters
+ * 
+ * Stores the parameters in a file as a serialized object
+ * 
+ */
   public void saveParameters() {
 	  try{
 		  OutputStream file = new FileOutputStream(nomeFich);
@@ -67,6 +83,12 @@ public class Option implements Serializable {
 	  }
   }
 
+  /**
+   * readParameters
+   * 
+   * Reads the parameters from a file with a serialized object
+   * 
+   */
   public void readParameters() {
     try{
       InputStream file = new FileInputStream(nomeFich);
@@ -84,51 +106,228 @@ public class Option implements Serializable {
     }
   }
   
+  /**
+   * getCanal()
+   * 
+   * @return Channels used
+   */
   public Canal getCanal() {return dados.canal;}
 
+  /**
+   * setNumFilterPoints
+   *   
+   * @param n - number of point for the moving average filter
+   */
   public void setNumFilterPoints(int n){ dados.numFilterPoints=n<1?1:n;}
+  
+  /**
+   * getNumFilterPoints
+   *   
+   * @return number of point for the moving average filter
+   */ 
   public int  getNumFilterPoints(){ return dados.numFilterPoints;}
 
+  /**
+   * setBaseLevel
+   * 
+   * @param n Minimum value, defines noise level
+   */
   public void setBaseLevel(double n){ dados.baseLevel=n<0?0:n;}
+  
+  /**
+   * getBaseLevel
+   * 
+   * @return Minimum noise threshold level
+   */
   public double getBaseLevel(){ return dados.baseLevel;}	  
   
+  /**
+   * setElevationMin
+   * 
+   * @param n Minimum elevation value
+   */
   public void setElevationMin(double n){ dados.elevationMin=n<0?0:n;}
+  
+  /**
+   * getElevationMin
+   * 
+   * @return Minimum elevation value
+   */
   public double getElevationMin(){ return dados.elevationMin;}
   
+  /**
+   * setDepthMin
+   * 
+   * @param n Minimum depth value
+   */
   public void setDepthMin(double n){ dados.depthMin=n<0?0:n;}
+  
+  /**
+   * getDepthMin
+   * 
+   * @return Minimum depth value
+   */
   public double getDepthMin(){ return dados.depthMin;}
    
+  /**
+   * setMarkSizeX
+   * 
+   * @param n Marking width
+   */
   public void setMarkSizeX(int n){ dados.markSizeX=n<0?0:n>MAX_MARK_SIZE?MAX_MARK_SIZE:n;}
+  
+  /**
+   * getMarkSizeX
+   * 
+   * @return Marking width
+   */
   public int getMarkSizeX(){ return dados.markSizeX;}
   
+  /**
+   * setMarkSizeY
+   * 
+   * @param n Marking height
+   */
   public void setMarkSizeY(int n){ dados.markSizeY=n<0?0:n>MAX_MARK_SIZE?MAX_MARK_SIZE:n;}
+  
+  /**
+   * getMarkSizeY
+   * 
+   * @return Marking height
+   */
   public int getMarkSizeY(){ return dados.markSizeY;}  
   
+  /**
+   * setMarkType
+   * 
+   * @param n Type of marking
+   */
   public void setMarkType(int n){ dados.markType=n;}
+  
+  /**
+   * getMarkType
+   * 
+   * @return Type of marking
+   */
   public int getMarkType(){ return dados.markType;}
+  
+  /**
+   * isMarkFill
+   * 
+   * @return True if type of marking if FILLED
+   */
   public boolean isMarkFill(){ return dados.markType==0;}
   
+  /**
+   * setMarkDraw
+   * 
+   * @param b True if mark is to be drawn
+   */
   public void setMarkDraw(boolean b){ dados.markDraw=b;}
+  
+  /**
+   * getMarkDraw
+   * 
+   * @return True if mark is to be drawn
+   */
   public boolean getMarkDraw(){ return dados.markDraw;}
   
+  /**
+   * setPlateau
+   * 
+   * @param b True to use segment with no inclination as significative for the algorithm
+   */
   public void setPlateau(boolean b){ dados.plateau=b;}
+  
+  /**
+   * getPlateau
+   * 
+   * @return True to use segment with no inclination as significative for the algorithm
+   */
   public boolean getPlateau(){ return dados.plateau;}
   
+  /**
+   * setMarkColorH
+   * 
+   * @param n Choose hill colour marking
+   */
   public void setMarkColorH(int n){ dados.markColorH=n;}
+  
+  /**
+   * getMarkColorH
+   * 
+   * @return Hill colour marking
+   */
   public int getMarkColorH(){ return dados.markColorH;}
+  
+  /**
+   * getColorH
+   * 
+   * @return Hill colour
+   */
   public Color getColorH(){ return cores[dados.markColorH];}
 
+  /**
+   * setMarkColorL
+   * 
+   * @param n Choose valley colour marking
+   */
   public void setMarkColorL(int n){ dados.markColorL=n;}
+  
+  /**
+   * getMarkColorL
+   * 
+   * @return Valley colour marking
+   */
   public int getMarkColorL(){ return dados.markColorL;}
+  
+  /**
+   * getColorL
+   * 
+   * @return Valley colour
+   */
   public Color getColorL(){ return cores[dados.markColorL];}
   
+  /**
+   * setDrawOriginal
+   * 
+   * @param b True if original profile is to be drawn
+   */
   public void setDrawOriginal(boolean b) {dados.drawOriginal=b;}
+  
+  /**
+   * getDrawOriginal
+   * 
+   * @return True if original profile is to be drawn in the resulting graph
+   */
   public boolean getDrawOriginal(){ return dados.drawOriginal;}
-
-  public double getGranularity() {return dados.granularity; }
+  
+  /**
+   * setGranularity
+   * 
+   * @param d Define value below which two values are indistinguishable
+   */
   public void setGranularity(double d) {dados.granularity=d<0?0:d;}
+  
+  /**
+   * getGranularity
+   * 
+   * @return Value below which two values are indistinguishable
+   */
+  public double getGranularity() {return dados.granularity; }
 
+  /**
+   * getHLline
+   * 
+   * @return True to draw line in resulting graph with hill and valley distribution
+   */
   public boolean getHLline() { return dados.hlLine;}
+  
+  /**
+   * setHLline
+   * 
+   * @param b True to draw line in resulting graph with hill and valley distribution
+   */
   public void setHLline(boolean b) { dados.hlLine=b;}  
   
 }
